6bb5717582854bc6915489d6cf0c85640612314a
Ge Yang <yangge1987@gmail.com>
2018-06-10 14:08:50 -0700

    add termcolor to dependencies

--- setup.py
+++ setup.py
@@ -21,5 +21,6 @@ setup(name="ml_logger",
           "Programming Language :: Python :: 3"
       ],
       packages=["ml_logger"],
-      install_requires=["typing", "numpy", "params_proto", "cloudpickle", "japronto", "uvloop==0.8.1", "requests", "requests_futures"]
+      install_requires=["typing", "numpy", "termcolor", "params_proto", "cloudpickle", "japronto", "uvloop==0.8.1", "requests",
+                        "requests_futures"]
       )
