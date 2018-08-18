f0e0bd63473402b3c9b78af37ae598bb8bca24e2
Ge Yang <yangge1987@gmail.com>
2018-08-17 22:37:38 -0700

    add comments to explain the API for the full-duplex task runner.
    Took 11 minutes

--- ml_logger/full_duplex.py
+++ ml_logger/full_duplex.py
@@ -6,9 +6,20 @@ from . import LogClient
 
 class Duplex(object):
     """ Duplex Agent
-    The full duplex agent should be started in the background.
+    The full duplex agent runs in the background. This is responsible for maintaining `alive` (running) state
+    when the main thread is running. This also allows duplex communication (lots of delay though) with the
+    server without using `server-sent-events` or `websocket`.
 
-    keey alive
+    When you call
+
+    ```
+    command, = logger.ping(message="running")
+    # or
+    commands = logger.ping(message="running", no_intercept=False)
+    ```
+
+    You can in fact get return commands. If the return command if "stop", the ping function raises an exception, and
+    stops the training.
     """
 
     def __init__(self, log_client: LogClient, prefix, keep_alive_interval=10, ):
