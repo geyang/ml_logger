46d3238980e0c292fa9b96641c5d5847f75ee3c6
Ge Yang <yangge1987@gmail.com>
2019-02-15 22:16:00 -0800

    fix launch scripts

--- ml-dash-client/scripts/generate_schema.py
+++ ml-dash-client/scripts/generate_schema.py
@@ -2,6 +2,6 @@ from graphql.utils import schema_printer
 from ml_dash.schema import schema
 
 if __name__ == "__main__":
-    with open("./schema.graphql", "w") as f:
+    with open("../src/data/schema.graphql", "w") as f:
         _ = schema_printer.print_schema(schema)
         f.write(_)
