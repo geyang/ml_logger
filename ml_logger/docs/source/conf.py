8e20a0fe331adcf518fe6b2688b971fe2f6f4e86
Ge Yang <geyang@fb.com>
2019-04-09 15:19:47 -0700

    use template index instead

--- ml_logger/docs/source/conf.py
+++ ml_logger/docs/source/conf.py
@@ -7,12 +7,14 @@ sys.path.insert(0, os.path.abspath('.'))
 project = 'ml-logger'
 copyright = '2019, Ge Yang'
 author = 'Ge Yang'
+#
+# with open('../../VERSION', encoding='utf-8') as f:
+#     # with open('../../VERSION', encoding='utf-8') as f:
+#     version = f.read()  # the short semver string
+#     release = version  # the release name (with beta/rc etc)
 
-with open('../../VERSION', encoding='utf-8') as f:
-    # with open('../../VERSION', encoding='utf-8') as f:
-    version = f.read()  # the short semver string
-    release = version  # the release name (with beta/rc etc)
-
+version = 'latest'
+release = 'latest'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -31,7 +33,8 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = [
-    '/home/docs/checkouts/readthedocs.org/user_builds/ml-logger/checkouts/latest/ml_logger/docs/source/',
+    '/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx',
+    # '/home/docs/checkouts/readthedocs.org/user_builds/ml-logger/checkouts/latest/ml_logger/docs/source/',
     '_templates'
 ]
 exclude_patterns = ['build']
