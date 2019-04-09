223009d4b3c2f506a4e117829124053ab056595f
Ge Yang <geyang@fb.com>
2019-04-09 15:11:48 -0700

    update conf

--- ml_logger/docs/source/conf.py
+++ ml_logger/docs/source/conf.py
@@ -1,37 +1,14 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
+import os
+import sys
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-import os, sys
-
-# --- configure the python environment for autodoc ---
-cwd = os.path.dirname(os.path.abspath(__file__))
-
-print(os.path.join(cwd, "../../"))
-# print(os.path.abspath("../../"))
-sys.path.insert(0, os.path.abspath(os.path.join(cwd, '../../')))
-# sys.path.insert(0, '../../')
-
-# -- Project information -----------------------------------------------------
+sys.path.insert(0, os.path.abspath('.'))
 
 project = 'ml-logger'
 copyright = '2019, Ge Yang'
 author = 'Ge Yang'
 
-print(os.path.join(cwd, "../../VERSION"))
-print(os.path.abspath("../../VERSION"))
-with open(os.path.abspath(os.path.join(cwd, '../../VERSION')), encoding='utf-8') as f:
+with open('../../VERSION', encoding='utf-8') as f:
     # with open('../../VERSION', encoding='utf-8') as f:
     version = f.read()  # the short semver string
     release = version  # the release name (with beta/rc etc)
@@ -53,23 +30,10 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['/home/docs/checkouts/readthedocs.org/user_builds/ml-logger/checkouts/latest/ml_logger/docs/source', '_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+templates_path = ['/home/docs/checkouts/readthedocs.org/user_builds/ml-logger/checkouts/latest/ml_logger/docs/source',
+                  '_templates']
+exclude_patterns = ['build']
 html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 # autodoc_mock_imports = ["waterbear"]
