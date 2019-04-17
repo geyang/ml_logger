5b3b197289669ef44dbd5a929bd736534208fa7a
Ge Yang <geyang@fb.com>
2019-04-16 20:07:29 -0700

    remove ml-vis-app. remove stale ml_dash project

--- ml-vis-app/src/lib/fetch-helper.js
+++
@@ -1,7 +0,0 @@
-export function status200(response) {
-    if (response.status < 300) {
-        return response;
-    } else {
-        throw new Error(response.statusText);
-    }
-}
--- ml_dash/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-LOCAL_TEST_DIR = '/tmp/ml-logger-debug'
-
-
-def pytest_addoption(parser):
-    parser.addoption('--log-dir', action='store', default=LOCAL_TEST_DIR,
-                     help="The logging path for the test.")
