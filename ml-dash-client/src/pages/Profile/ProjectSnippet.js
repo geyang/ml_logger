d0f2448508883ff3ecdcf7d37345cdaf2aab1015
Ge Yang <geyang@fb.com>
2019-06-01 21:25:40 -0700

    add dropshadow to ProjectSnippet

--- ml-dash-client/src/pages/Profile/ProjectSnippet.js
+++ ml-dash-client/src/pages/Profile/ProjectSnippet.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import {Link} from 'found';
 import {Anchor, Button, Heading} from 'grommet';
+import styled from "styled-components";
+
+const StyledProjectSnippet = styled(Link)`
+      display: block;
+      background: white;
+      padding: 10px;
+      border-radius: 10px;
+      &:hover {
+        box-shadow: 0 0 40px rgba(0, 0, 0, 0.04);
+      }
+      text-decoration: none;
+      color: #444;
+`;
 
 export default function ProjectSnippet({username, name}) {
-  // const {username} = this.props.match.params;
   return (
-      <Button as={Link} to={"/" + username + "/" + name + '/'}
-              style={{
-                display: "block",
-                background: "white",
-                padding: "10px",
-                borderRadius: "10px"
-              }}
-              padding='medium'>
+      <StyledProjectSnippet to={"/" + username + "/" + name + '/'} padding='medium'>
         <Heading level={4} style={{margin: 0}}>{name}</Heading>
-      </Button>
+      </StyledProjectSnippet>
   );
 }
