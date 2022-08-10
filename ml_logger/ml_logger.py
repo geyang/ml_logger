fix the image normalization bug

ml_logger/ml_logger.py
@@ -1447,8 +1447,8 @@ class ML_Logger:
         s3 = boto3.client('s3')
         return s3.download_file(bucket, object_name, to)
 
-    def save_images(self, stack, key, n_rows=None, n_cols=None, cmap=None, normalize=None, value_range=(0, 1),
-                    background=1):
+    def save_images(self, stack, key, n_rows=None, n_cols=None, cmap=None, normalize=None,
+                    value_range=(0, 1), background=1):
         """Log images as a composite of a grid. Images input as a 4-D stack.
 
         :param stack: Size(n, w, h, c)
@@ -1473,15 +1473,15 @@ class ML_Logger:
             from matplotlib import cm
             map_fn = cm.get_cmap(cmap or 'Greys')
 
-            if normalize is None or normalize is False:
+            if normalize in [None, False]:
                 pass
             else:
                 if normalize == 'grid':
                     low = np.nammin(stack)
                     high = np.nanmax(stack)
                 elif normalize in [True, 'individual']:
-                    low = np.nammin(stack, axis=(1, 2))[None, ...]
-                    high = np.nammax(stack, axis=(1, 2))[None, ...]
+                    low = np.nanmin(stack, axis=(1, 2))[None, ...]
+                    high = np.nanmax(stack, axis=(1, 2))[None, ...]
                 elif isinstance(normalize, Sequence) and not isinstance(normalize, str):
                     low, high = normalize
                 else:
@@ -1491,7 +1491,7 @@ class ML_Logger:
                 stack = (stack - low) / (r if r.all() else 1)
                 stack = stack * (value_high - value_low) + value_low
 
-            stack = stack.clip(value_low, value_high) * 255
+            stack = (map_fn(stack.clip(value_low, value_high)) * 255).astype(np.uint8)
             stack = stack.astype(np.uint8)
 
         elif len(stack.shape) == 4:

ml_logger_tests/test_ml_logger.py
@@ -199,25 +199,37 @@ def test_yaml(setup):
 
 def test_image(setup):
     import scipy.misc
-    import numpy as np
 
-    image_bw = scipy.misc.face(gray=True)[::4, ::4]
     face_rgba = scipy.misc.face()
-    face_rgb = face_rgba[::4, ::4, :3]
 
-    # zero_rgb = np.zeros((64, 64, 3), dtype=np.uint8)
-
-    # logger.save_image(image_bw, "bw_face.png", normalize='individual')
-    path = logger.save_image(face_rgba / 255, 'rgb.png', normalize='grid')
-    path = logger.save_image(face_rgba / 1000, f'rgba_face_{100}.png', normalize=False)
+    logger.save_image(face_rgba / 255, 'rgb.png', normalize='grid')
+    logger.save_image(face_rgba / 1000, f'rgba_face_{100}.png', normalize=False)
     path = logger.save_image(face_rgba / 1000, f'rgba_face_{100}_normalized.png', normalize="individual")
     print(f"files:/{logger.root}/{logger.prefix}/{path}")
 
-    # image_zero = np.zeros((64, 64, 1), dtype=np.uint8)
-    # logger.save_image(image_zero, f"bw_{100}.png")
 
-    # logger.save_image(image_zero[:, :, 0].astype(np.float32), "black_white_individual.png", normalize='individual')
-    # logger.save_image(np.ones([64, 64]), "black_white_grid.png", normalize='grid')
+def test_image_cmap(setup):
+    import scipy.misc
+    import numpy as np
+
+    face_bw = scipy.misc.face().mean(axis=-1)
+    # the image dtype can not be uint8 --- in that case no cmap is applied
+    logger.save_image(face_bw, f"face_bw_cmap.png", cmap="cool", normalize=True)
+    logger.save_image(face_bw.astype(np.uint8), f"face_bw_no_cmap.png")
+
+
+def test_image_zero_norm(setup):
+    import numpy as np
+
+    image_zero = np.zeros((64, 64, 1))
+    logger.save_image(image_zero, f"zero_{100}.png", normalize="grid")
+
+
+def test_image_normalization(setup):
+    import numpy as np
+
+    image_zero = np.zeros((64, 64, 1))
+    logger.save_image(image_zero, f"zero_{100}.png", normalize="grid")
 
 
 def test_pyplot(setup):